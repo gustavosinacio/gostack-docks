ef5a15fbdd13c26d45db8e27feca1c234046074e
gustavo <gsinacio94@gmail.com>
2019-07-01 00:57:35 -0300

    Undoing linux-config merge

--- apt-installs.sh
+++
@@ -1,6 +0,0 @@
-sudo apt install -y curl git terminator chrome-gnome-shell gnome-shell-extensions vlc transmission gparted
-# sudo apt install git -y
-# sudo apt install terminator -y
-# sudo apt install chrome-gnome-shell -y 
-# sudo apt install gnome-shell-extensions -y
-# sudo apt install vlc -y
--- chrome/config.sh
+++
@@ -1,7 +0,0 @@
-echo "--------------- Chrome ---------------"
-mkdir download
-cd download
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i *.deb
-cd ..
-rm -r download
